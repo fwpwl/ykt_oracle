cc963814cd68ee53f827e0becb3d7c2257bb0391
fwp <fwp@myhost.local>
2020-02-13 20:01:25 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_urls/imust_urls.py
+++ proxy/ykt_proxy/data_transfer/module_urls/imust_urls.py
@@ -2,10 +2,10 @@
 from django.conf.urls import url
 
 from data_transfer.module_views.imust_views import imust_get_course_data_view, imust_get_choose_data_view, \
-    imust_get_student_data_view, imust_get_teacher_data_view
+    imust_get_user_data_view
 
 urlpatterns = [
-    url(r'^get_user_data/?$', imust_get_student_data_view),
+    url(r'^get_user_data/?$', imust_get_user_data_view),
     url(r'^get_course_data/?$', imust_get_course_data_view),
     url(r'^get_choose_data/?$', imust_get_choose_data_view),
 
