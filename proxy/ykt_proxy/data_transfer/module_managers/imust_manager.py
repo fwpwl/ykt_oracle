6964712fdb64c624b0ff2a375d7fa37b92bd5bc2
fwp <fwp@myhost.local>
2020-02-13 21:13:16 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/imust_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/imust_manager.py
@@ -38,7 +38,7 @@ def imust_get_department_data():
 
 def imust_get_tra_data():
     statement = "select SSXY, ZY, bjmc, rxxn from xzbjb"
-    data_list = db.get_raw_data_by_statement(statement=statement, var_tuple=None)
+    data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
     keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
     final_info_list = query_data_to_dict_list(data_list, keys_list)
     return final_info_list
