c0234df7d0a93061faf3d482d00795f6d481143f
fwp <fwp@myhost.local>
2020-02-13 10:49:54 +0800

    fix bug

---
+++ proxy/ykt_proxy/data_transfer/module_views/sdada_views.py
@@ -0,0 +1,66 @@
+# coding:utf-8
+
+from data_transfer.module_managers.sdada_manager import sdada_get_course_data, sdada_get_choose_data, \
+    is_valid_request, sdada_get_student_data, sdada_get_teacher_data
+from data_transfer.utils.network import success_response, get_para_from_request_safe, error_response
+
+
+def sdada_get_student_data_view(request):
+    """
+    URL[GET]:/data/sdada/get_student_data/
+    """
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
+
+    ret_data = sdada_get_student_data()
+    return success_response(ret_data)
+
+
+def sdada_get_teacher_data_view(request):
+    """
+    URL[GET]:/data/sdada/get_teacher_data/
+    """
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
+
+    ret_data = sdada_get_teacher_data()
+    return success_response(ret_data)
+
+
+def sdada_get_course_data_view(request):
+    """
+    URL[GET]:/data/sdada/get_course_data/
+    PARA:
+    key
+    year
+    term
+    """
+    key = get_para_from_request_safe(request, 'key')
+    year = get_para_from_request_safe(request, 'year')
+    term = get_para_from_request_safe(request, 'term')
+
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
+
+    ret_data = sdada_get_course_data(year, term)
+    return success_response(ret_data)
+
+
+def sdada_get_choose_data_view(request):
+    """
+    URL[GET]:/data/sdada/get_choose_data/
+    PARA:
+    key
+    year_str
+    """
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
+
+    year = get_para_from_request_safe(request, 'year')
+    term = get_para_from_request_safe(request, 'term')
+
+    ret_data = sdada_get_choose_data(year, term)
+    return success_response(ret_data)
