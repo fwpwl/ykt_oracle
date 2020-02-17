187b982edf6f585c13cc5fddc70e2a1612fea56c
fwp <fwp@myhost.local>
2020-02-17 16:46:21 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/sdada_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/sdada_manager.py
@@ -75,6 +75,9 @@ def sdada_get_gra_student_data():
 
 
 def sdada_get_teacher_data():
+    """
+    ryzt: 1=在职
+    """
     statement = "select DWMC, XM, ZGH from v_jzg where Ryzt=1"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
     keys_list = ["department_name", 'name', 'number']
@@ -87,16 +90,20 @@ def sdada_get_course_data(year_term):
     jx0404id: 教学班
     Jxbid： 行政班id
     """
-    statement = "select Jxbid, Kch, Kcm, jx0404id, ktmc, Bh, Xm, Xnxqdm from v_it_kcb_kcbxx where Xnxqdm='{}'".format(year_term)
+    statement = "select Kch, Kcm, jx0404id, ktmc, Bh, Xm, Xnxqdm from v_it_kcb_kcbxx where Xnxqdm='{}'".format(year_term)
 
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", "course_code", "course_name", 'classroom_code', "classroom_name", "teacher_number",
+    keys_list = ["course_code", "course_name", 'classroom_code', "classroom_name", "teacher_number",
                  "teacher_name", "year_term"]
     final_info_list = query_data_to_dict_list(data_list, keys_list)
     return final_info_list
 
 
+
 def sdada_get_choose_data(year_term):
+    """
+
+    """
     statement = "select jx0404id, XH from v_it_kcb_xxkb where Xnxq='{}'".format(year_term)
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
     keys_list = ["classroom_code", "student_number"]
