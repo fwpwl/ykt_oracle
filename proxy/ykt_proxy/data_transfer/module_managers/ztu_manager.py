e9b4d59595dc119416355a487b376a050e632eee
fwp <fwp@myhost.local>
2020-02-19 17:23:04 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/ztu_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/ztu_manager.py
@@ -37,9 +37,9 @@ def ztu_get_department_data():
     return final_info_list
 
 def ztu_get_tra_data():
-    statement = "select XSM, ZYM, BJH, RXNJ from zxbjb"
+    statement = "select SSXY, XW, BJMC, RXXN from zxbjb"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
+    keys_list = ["department_name", "major", 'tra_classroom_name', 'year']
     final_info_list = query_data_to_dict_list(data_list, keys_list)
     return final_info_list
 
