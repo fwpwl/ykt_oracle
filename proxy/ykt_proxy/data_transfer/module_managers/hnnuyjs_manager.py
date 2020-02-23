9317162ac20cc8532034757c43688e58c4580774
fwp <fwp@myhost.local>
2020-02-24 02:34:44 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/hnnuyjs_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/hnnuyjs_manager.py
@@ -30,9 +30,9 @@ def query_data_to_dict_list(query_data_list_of_tuple, keys_list):
 
 
 def hnnuyjs_get_department_data():
-    statement = "select xymc from hunnu_share.yks_ykt_xyxxb"
+    statement = "select xymc,xydm,id from hunnu_share.yks_ykt_xyxxb"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", ]
+    keys_list = ["department_name", 'department_code', 'id']
     final_info_list = query_data_to_dict_list(data_list, keys_list)
     return final_info_list
 
