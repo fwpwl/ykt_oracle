eaf88e967bbed22923117a845d46c1f71faaaec1
fwp <fwp@myhost.local>
2020-02-24 02:21:18 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/hnnuyjs_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/hnnuyjs_manager.py
@@ -37,11 +37,11 @@ def hnnuyjs_get_department_data():
     return final_info_list
 
 def hnnuyjs_get_tra_data():
-    statement = "select ssxy, ZY, bjmc, RXNJ from hunnu_share.yks_ykt_xyxxb"
-    data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
-    final_info_list = query_data_to_dict_list(data_list, keys_list)
-    return final_info_list
+    # statement = "select ssxy, ZY, bjmc, RXNJ from hunnu_share.yks_ykt_xyxxb"
+    # data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
+    # keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
+    # final_info_list = query_data_to_dict_list(data_list, keys_list)
+    return []
 
 def hnnuyjs_get_user_data():
     statement = "select SSXY, XM, XH, SF, RXNY from hunnu_share.yks_ykt_rysj"
