7ec9cec3d97cc2aa6d5ded347001e490ee4bd06a
fwp <fwp@myhost.local>
2020-03-03 20:32:20 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/hnit_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/hnit_manager.py
@@ -28,7 +28,17 @@ def query_data_to_dict_list(query_data_list_of_tuple, keys_list):
         final_list.append(tmp_dict)
     return final_list
 
-department_dict = {}
+
+def get_department():
+    department_dict = {}
+    statement = "select xymc, DWH from LY_YKT_VIEW_XYXXB"
+    data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
+    keys_list = ["department_name", "ID"]
+    final_info_list = query_data_to_dict_list(data_list, keys_list)
+    for k in final_info_list:
+        department_dict[k['ID']] = k['department_name']
+    return department_dict
+
 def hnit_get_department_data():
     statement = "select xymc, DWH from LY_YKT_VIEW_XYXXB"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
@@ -39,21 +49,25 @@ def hnit_get_department_data():
     return final_info_list
 
 def hnit_get_tra_data():
+    department_dict = get_department()
     statement = "select SSYX, ZY, BJMC, RXXN from LY_YKT_VIEW_XZBJB"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
+    keys_list = ["department_id", "major", 'tra_classroom_name', "year"]
     final_info_list = query_data_to_dict_list(data_list, keys_list)
+    for k in final_info_list:
+        k['department_name'] = department_dict[k['department_id']]
     return final_info_list
 
 def hnit_get_user_data():
+    department_dict = get_department()
     teachers = get_teacher_data()
-    statement = "select SSY, xzbjmc, XM, XH, sf, rxxn from LY_YKT_VIEW_XSYHXX"
+    statement = "select SSYX, xzbjmc, XM, XH, sf, rxxn from LY_YKT_VIEW_XSYHXX"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
-    keys_list = ["department_name", "tra_class_name", 'name', 'number', 'user_type', 'year']
+    keys_list = ["department_id", "tra_class_name", 'name', 'number', 'user_type', 'year']
     final_info_list = query_data_to_dict_list(data_list, keys_list)
-    # for k in final_info_list:
-    #     pass
-    return final_info_list
+    for k in final_info_list + teachers:
+        k['department_name'] = department_dict[k['department_id']]
+    return final_info_list + teachers
 
 def get_teacher_data():
     statement = "select SSYX, XM, XH, sf from LY_YKT_VIEW_JZGJBXX"
@@ -61,16 +75,19 @@ def get_teacher_data():
     keys_list = ["department_name", 'name', 'number', 'user_type']
     final_info_list = query_data_to_dict_list(data_list, keys_list)
     for k in final_info_list:
-        k['tra_classroom_name'] = ''
+        k['tra_classroom_name'] = None
         k['year'] = 0
     return final_info_list
 
 def hnit_get_course_data(year, term):
+    department_dict = get_department()
     statement = "select SSYX, kch, kcmc, xkh, kcbjmc, jsgh, jsxm, KKXN, KKXQ from LY_YKT_VIEW_BXQKKXXB where KKXN='{}' and KKXQ='{}'".format(year, term)
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
     keys_list = ["department_name", "course_code", "course_name", 'classroom_code', "classroom_name", 
         "teacher_number", "teacher_name", "year", "term"]
     final_info_list = query_data_to_dict_list(data_list, keys_list)
+    for k in final_info_list:
+        k['department_name'] = department_dict[k['department_id']]
     return final_info_list
 
 def hnit_get_choose_data(year, term):
