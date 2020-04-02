e27a73475369eae1d0b821715fcc40dfe0c73fb3
fwp <fwp@myhost.local>
2020-04-02 21:58:51 +0800

    fix bug

--- proxy/ykt_proxy/data_transfer/module_managers/hainnu_manager.py
+++ proxy/ykt_proxy/data_transfer/module_managers/hainnu_manager.py
@@ -37,7 +37,7 @@ def hainnu_get_department_data():
     return final_info_list
 
 def hainnu_get_tra_data():
-    statement = "select ssxy, ZY, bjmc, RXXN from xzbjb"
+    statement = "select SSXY, SSZY, bjmc, RXXN from xzbjb"
     data_list = get_db_client().get_raw_data_by_statement(statement=statement, var_tuple=None)
     keys_list = ["department_name", "major", 'tra_classroom_name', "year"]
     final_info_list = query_data_to_dict_list(data_list, keys_list)
